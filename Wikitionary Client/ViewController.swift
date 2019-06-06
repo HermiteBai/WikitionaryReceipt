cad38ebf07988e80144910f7149f498d57f8df59
Lanxiao Bai <hermitebai@outlook.com>
2019-06-06 12:57:22 -0500

    Init Project

---
+++ Wikitionary Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wikitionary Client
+//
+//  Created by Lanxiao Bai on 6/6/19.
+//  Copyright © 2019 HermiteBai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
